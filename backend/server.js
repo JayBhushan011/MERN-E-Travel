65a9d4fbdc0a3b5261cb49e265a188b691e60c43
JAY BHUSHAN <jaybhushan011@gmail.com>
2020-11-09 22:28:34 +0530

    Backend

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require('cors');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000
+
+
+app.use(cors());
+app.use(express.json());
+
+// Mongoose MongoDB Database Setup
+require('dotenv').config();
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+// *********
+
+
+const tripRouter = require('./routes/trip');
+app.use('/trip', tripRouter);
+
+const userRouter = require('./routes/user');
+app.use('/user', userRouter);
+
+
+app.get("/", function(request, response) {
+  response.send("<h1>Hello World!</h1>");
+})
+
+app.listen(3000, function() {
+  console.log("Server is running on port " + 3000);
+});
